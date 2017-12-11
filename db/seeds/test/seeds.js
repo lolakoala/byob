978fefa85d053cb629b3007132554b4c3cb0f414
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-11 14:34:32 -0700

    create boiler plate for seeding data in dev and test

---
+++ db/seeds/test/seeds.js
@@ -0,0 +1,37 @@
+export.seeds = function (knex, Promise) {
+  return knex('bulletins').del()
+    .then(() => knex('choirs')).del()
+    .then(() => knex('bills')).del()
+    .then(() => knex('users')).del()
+    .then(() => knex('houses')).del()
+    .then(() => {
+      return Promise.all([
+        knex('houses').insert([
+          //houses seed data goes here
+        ])
+        .then(() => {
+          return knex('users').insert([
+            //bulletins seed data goes here
+          ])
+        })
+        .then(() => {
+          return knex('bills').insert([
+            //bills seed data goes here
+          ])
+        })
+        .then(() => {
+          return knex('choirs').insert([
+            //choirs seed data goes here
+          ])
+        })
+        .then(() => {
+          return knex('bulletins').insert([
+            //bulletins seed data goes here
+          ])
+        })
+        .then(() => console.log('Test Seeding Complete!'))
+        .catch(error => console.log({ error }))
+      ])//closes Promise.all//
+    })
+    .catch(error => console.log({ error }))
+}
