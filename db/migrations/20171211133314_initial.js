f4552172214952ae18540bfe563beea61debf5c6
Lola Brenner <amandadbrenner@gmail.com>
2017-12-11 13:43:05 -0700

    adds bulletins table

--- db/migrations/20171211133314_initial.js
+++ db/migrations/20171211133314_initial.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('houses', function(table) {
@@ -33,12 +32,10 @@ exports.up = function(knex, Promise) {
       table.foreign('houseId').references('houses.id');
       table.timestamps(true, true);
     }),
-    knex.schema.createTable('chores', function(table) {
+    knex.schema.createTable('bulletins', function(table) {
       table.increments('id').primary();
-      table.string('name');
-      table.string('details');
-      table.integer('userId').unsigned();
-      table.foreign('userId').references('users.id');
+      table.string('title');
+      table.string('body');
       table.integer('houseId').unsigned();
       table.foreign('houseId').references('houses.id');
       table.timestamps(true, true);
@@ -47,36 +44,11 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-
-};
-
-
-
-exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('projects', function(table) {
-      table.increments('id').primary();
-      table.string('title');
-      table.timestamps(true, true);
-    }),
-    knex.schema.createTable('palettes', function(table) {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('color1');
-      table.string('color2');
-      table.string('color3');
-      table.string('color4');
-      table.string('color5');
-      table.integer('projectId').unsigned();
-      table.foreign('projectId').references('projects.id');
-      table.timestamps(true, true);
-    })
-  ]);
-};
-
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('palettes'),
-    knex.schema.dropTable('projects')
+    knex.schema.dropTable('bulletins'),
+    knex.schema.dropTable('chores'),
+    knex.schema.dropTable('bills'),
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('houses')
   ]);
 };
