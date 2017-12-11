458e82b96716587ec01939220855123e0dcc8027
Lola Brenner <amandadbrenner@gmail.com>
2017-12-11 14:47:04 -0700

    fixes linter errors in seed data

--- db/seeds/dev/seeds.js
+++ db/seeds/dev/seeds.js
@@ -1,4 +1,4 @@
-export.seeds = function (knex, Promise) {
+exports.seeds = function (knex, Promise) {
   return knex('bulletins').del()
     .then(() => knex('chores')).del()
     .then(() => knex('bills')).del()
@@ -16,62 +16,62 @@ export.seeds = function (knex, Promise) {
           { name: 'FrogHouse', secretKey: 'oiajfoiuw'},
           { name: 'SheepHouse', secretKey: '08935jneg'}
         ])
-        .then(() => {
-          return knex('users').insert([
-            { name: 'Lola', houseId: 1 },
-            { name: 'Nik', houseId: 1 },
-            { name: 'Rufus', houseId: 1 },
-            { name: 'Adam', houseId: 1 },
-            { name: 'Alex', houseId: 1 },
-            { name: 'Amy', houseId: 2 },
-            { name: 'Francy', houseId: 2 },
-            { name: 'Robbie', houseId: 2 },
-            { name: 'Brittany', houseId: 2 },
-            { name: 'Hector', houseId: 3 },
-            { name: 'Luke', houseId: 3 },
-            { name: 'Rufus', houseId: 3 },
-            { name: 'Jenn', houseId: 3 },
-            { name: 'Rhonda', houseId: 3 },
-            { name: 'Melissa', houseId: 4 },
-            { name: 'Kayla', houseId: 4 },
-            { name: 'Laurel', houseId: 4 },
-            { name: 'Schweetz', houseId: 5 },
-            { name: 'Ralphie', houseId: 5 },
-            { name: 'Porter', houseId: 5 },
-            { name: 'Bandit', houseId: 5 },
-            { name: 'Fendie', houseId: 5 },
-            { name: 'Julie', houseId: 6 },
-            { name: 'Leta', houseId: 6 },
-            { name: 'Nate', houseId: 6 },
-            { name: 'Jhun', houseId: 7 },
-            { name: 'Christy', houseId: 7 },
-            { name: 'Louisa', houseId: 7 },
-            { name: 'Will', houseId: 7 },
-            { name: 'Pam', houseId: 7 },
-            { name: 'Mike', houseId: 8 },
-            { name: 'Brenna', houseId: 8 },
-            { name: 'Meeka', houseId: 8 },
-            { name: 'Beyonce', houseId: 8 }
-          ])
-        })
-        .then(() => {
-          return knex('bills').insert([
-            //bills seed data goes here
-          ])
-        })
-        .then(() => {
-          return knex('chores').insert([
-            //chores seed data goes here
-          ])
-        })
-        .then(() => {
-          return knex('bulletins').insert([
-            //bulletins seed data goes here
-          ])
-        })
-        .then(() => console.log('Dev Seeding Complete!'))
-        .catch(error => console.log({ error }))
-      ])//closes Promise.all//
+          .then(() => {
+            return knex('users').insert([
+              { name: 'Lola', houseId: 1 },
+              { name: 'Nik', houseId: 1 },
+              { name: 'Rufus', houseId: 1 },
+              { name: 'Adam', houseId: 1 },
+              { name: 'Alex', houseId: 1 },
+              { name: 'Amy', houseId: 2 },
+              { name: 'Francy', houseId: 2 },
+              { name: 'Robbie', houseId: 2 },
+              { name: 'Brittany', houseId: 2 },
+              { name: 'Hector', houseId: 3 },
+              { name: 'Luke', houseId: 3 },
+              { name: 'Rufus', houseId: 3 },
+              { name: 'Jenn', houseId: 3 },
+              { name: 'Rhonda', houseId: 3 },
+              { name: 'Melissa', houseId: 4 },
+              { name: 'Kayla', houseId: 4 },
+              { name: 'Laurel', houseId: 4 },
+              { name: 'Schweetz', houseId: 5 },
+              { name: 'Ralphie', houseId: 5 },
+              { name: 'Porter', houseId: 5 },
+              { name: 'Bandit', houseId: 5 },
+              { name: 'Fendie', houseId: 5 },
+              { name: 'Julie', houseId: 6 },
+              { name: 'Leta', houseId: 6 },
+              { name: 'Nate', houseId: 6 },
+              { name: 'Jhun', houseId: 7 },
+              { name: 'Christy', houseId: 7 },
+              { name: 'Louisa', houseId: 7 },
+              { name: 'Will', houseId: 7 },
+              { name: 'Pam', houseId: 7 },
+              { name: 'Mike', houseId: 8 },
+              { name: 'Brenna', houseId: 8 },
+              { name: 'Meeka', houseId: 8 },
+              { name: 'Beyonce', houseId: 8 }
+            ]);
+          })
+          .then(() => {
+            return knex('bills').insert([
+              //bills seed data goes here
+            ]);
+          })
+          .then(() => {
+            return knex('chores').insert([
+              //chores seed data goes here
+            ]);
+          })
+          .then(() => {
+            return knex('bulletins').insert([
+              //bulletins seed data goes here
+            ]);
+          })
+          .then(() => console.log('Dev Seeding Complete!'))
+          .catch(error => console.log({ error }))
+      ]);//closes Promise.all//
     })
-    .catch(error => console.log({ error }))
-}
+    .catch(error => console.log({ error }));
+};
