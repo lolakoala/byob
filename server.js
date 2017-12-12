e4c7c95e4ce789cc56871818bec6198944ca55e3
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-12 10:56:56 -0700

    begin writing get and delete endpoints

--- server.js
+++ server.js
@@ -23,6 +23,97 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/v1/houses', (request, response) => {
+  database('houses').select()
+    .then(houses => {
+      return response.status(200).json(houses);
+    })
+    .catch( error => {
+      return response.status(500).json({ error });
+    });
+});
+
+
+app.get('/api/v1/houses/:id/bills', (request, response) => {
+  database('bills').select()
+    .then(bills => {
+      return response.status(200).json(bills);
+    })
+    .catch( error => {
+      return response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/houses/:id/chores', (request, response) => {
+  database('chores').select()
+    .then(chores => {
+      return response.status(200).json(chores);
+    })
+    .catch( error => {
+      return response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/houses/:id/bulletins', (request, response) => {
+  database('bulletins').select()
+    .then(bulletins => {
+      return response.status(200).json(bulletins);
+    })
+    .catch( error => {
+      return response.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/houses/:id/bills/:id', (request, response) => {
+  const { id } = request.params;
+  database('bills').where('id', id).select()
+    .then(bills => {
+      if (!bills.length) {
+        return response.status(422).json({
+          error: `Could not find a bill with an id of ${id}.`
+        });
+      }
+      databse('bills').where('id', id).del()
+        .then(() => {
+          return response.status(500).json({ error });
+        });
+    });
+});
+
+app.delete('/api/v1/houses/:id/chores/:id', (request, response) => {
+  const { id } = request.params;
+  database('chores').where('id', id).select()
+    .then(chores => {
+      if (!chores.length) {
+        return response.status(422).json({
+          error: `Could not find a bill with an id of ${id}.`
+        });
+      }
+      databse('chores').where('id', id).del()
+        .then(() => {
+          return response.status(500).json({ error });
+        });
+    });
+});
+
+app.delete('/api/v1/houses/:id/bulletins/:id', (request, response) => {
+  const { id } = request.params;
+  database('bulletins').where('id', id).select()
+    .then(bulletins => {
+      if (!bulletins.length) {
+        return response.status(422).json({
+          error: `Could not find a bill with an id of ${id}.`
+        });
+      }
+      databse('bulletins').where('id', id).del()
+        .then(() => {
+          return response.status(500).json({ error });
+        });
+    });
+});
+
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
