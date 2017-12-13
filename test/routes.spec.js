1cee7bbeab76b195d0cd2c7ff6326de8d1c3a854
Lola Brenner <amandadbrenner@gmail.com>
2017-12-12 17:50:32 -0700

    adds placeholder for every endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -21,4 +21,83 @@ describe('API Routes', (done) => {
       .catch(error => { throw error; });
   });
 
+  describe('GET /api/v1/houses', () => {
+
+  });
+
+  describe('GET /api/v1/users', () => {
+
+  });
+
+  describe('GET /api/v1/users/:id', () => {
+
+  });
+
+  describe('GET /api/v1/houses/:id/users', () => {
+
+  });
+
+  describe('GET /api/v1/houses/:houseId/bills', () => {
+
+  });
+
+  describe('GET /api/v1/houses/:houseId/chores', () => {
+
+  });
+
+  describe('GET /api/v1/houses/:houseId/bulletins', () => {
+
+  });
+
+  describe('POST /api/v1/houses', () => {
+
+  });
+
+  describe('POST /api/v1/houses/:houseId/users', () => {
+
+  });
+
+  describe('POST /api/v1/houses/:houseId/bills', () => {
+
+  });
+
+  describe('POST /api/v1/houses/:houseId/chores', () => {
+
+  });
+
+  describe('POST /api/v1/houses/:houseId/bulletins', () => {
+
+  });
+
+  describe('PATCH /api/v1/houses/:id', () => {
+
+  });
+
+  describe('PATCH /api/v1/users/:id', () => {
+
+  });
+
+  describe('PATCH /api/v1/houses/:houseId/bills/:id', () => {
+
+  });
+
+  describe('PATCH /api/v1/houses/:houseId/chores/:id', () => {
+
+  });
+
+  describe('PATCH /api/v1/houses/:houseId/bulletins/:id', () => {
+
+  });
+
+  describe('DELETE /api/v1/houses/:houseId/bills/:id', () => {
+
+  });
+
+  describe('DELETE /api/v1/houses/:houseId/chores/:id', () => {
+
+  });
+
+  describe('DELETE /api/v1/houses/:houseId/bulletins/:id', () => {
+
+  });
 });
