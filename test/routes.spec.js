c48b02e9ab80951bf13fcf18392a5aa546bad42c
Lola Brenner <amandadbrenner@gmail.com>
2017-12-12 17:41:15 -0700

    boiler plate for testing file

---
+++ test/routes.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const knex = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('API Routes', (done) => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => done())
+      .catch(error => { throw error; });
+  });
+
+  beforeEach((done) => {
+    knex.seed.run()
+      .then(() => done())
+      .catch(error => { throw error; });
+  });
+
+});
