71b9a4a2262904e0a06d4ec25a36aa5815a9a13f
Lola Brenner <amandadbrenner@gmail.com>
2017-12-14 06:51:08 -0700

    adds happy and sad path test for delete chore

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -320,10 +320,30 @@ describe('API Routes', (done) => {
         .catch(error => { throw error; });
     });
   });
-  //
-  // describe('DELETE /api/v1/houses/:houseId/chores/:id', () => {
-  //
-  // });
+
+  describe('DELETE /api/v1/houses/:houseId/chores/:id', () => {
+    it('should delete chore with id specified', () => {
+      return chai.request(server)
+        .del('/api/v1/houses/2/chores/2')
+        .then(response => {
+          response.should.have.status(204);
+        })
+        .catch(error => { throw error; });
+    });
+
+    it('should return 422 if no chore found', () => {
+      return chai.request(server)
+        .del('/api/v1/houses/2/chores/9898')
+        .then(response => {
+          response.should.have.status(422);
+          response.should.be.json;
+          response.body.should.be.a('object');
+          response.body.should.have.property('error');
+          response.body.error.should.equal(`Could not find a chore with an id of 9898.`);
+        })
+        .catch(error => { throw error; });
+    });
+  });
   //
   // describe('DELETE /api/v1/houses/:houseId/bulletins/:id', () => {
   //
