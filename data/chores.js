12be25b53f2e148e5bb24e6ab72f331cbc583156
Lola Brenner <amandadbrenner@gmail.com>
2017-12-14 14:34:59 -0700

    seeds houses and users

---
+++ data/chores.js
@@ -0,0 +1,188 @@
+module.exports = [
+  {
+    name: 'sweep the front deck',
+    details: 'get this done today!',
+    userId: 1,
+    house: 'DogHouse'
+  },
+  {
+    name: 'sweep the back deck',
+    details: 'get this done tomorrow!',
+    userId: 2,
+    house: 'CatHouse'
+  },
+  {
+    name: 'sweep the stairs',
+    details: 'get this done now!',
+    userId: 3,
+    house: 'BirdHouse'
+  },
+  {
+    name: 'take out the front trash',
+    details: 'it smells horrible!',
+    userId: 4,
+    house: 'BugHouse'
+  },
+  {
+    name: 'take out the back trash',
+    details: 'it smells horrible!',
+    userId: 5,
+    house: 'MouseHouse'
+  },
+  {
+    name: 'take out the kitchen trash',
+    details: 'it is full!',
+    userId: 6,
+    house: 'SnakeHouse'
+  },
+  {
+    name: 'take out the bathroom trash',
+    details: 'it is full!',
+    userId: 7,
+    house: 'FrogHouse'
+  },
+  {
+    name: 'take out the bedroom trash',
+    details: 'it is full!',
+    userId: 8,
+    house: 'SheepHouse'
+  },
+  {
+    name: 'clean the entryway',
+    details: 'it is a mess!',
+    userId: 9,
+    house: 'DogHouse'
+  },
+  {
+    name: 'clean the office',
+    details: 'it is a mess!',
+    userId: 10,
+    house: 'CatHouse'
+  },
+  {
+    name: 'clean the front bedroom',
+    details: 'it is a mess!',
+    userId: 11,
+    house: 'BirdHouse'
+  },
+  {
+    name: 'clean the side bedroom',
+    details: 'it is a mess!',
+    userId: 12,
+    house: 'BugHouse'
+  },
+  {
+    name: 'clean the back bedroom',
+    details: 'it is a mess!',
+    userId: 13,
+    house: 'MouseHouse'
+  },
+  {
+    name: 'clean the guest bedroom',
+    details: 'it is a mess!',
+    userId: 14,
+    house: 'SnakeHouse'
+  },
+  {
+    name: 'clean the master bedroom',
+    details: 'it is a mess!',
+    userId: 15,
+    house: 'FrogHouse'
+  },
+  {
+    name: 'clean the front bathroom',
+    details: 'it is a mess!',
+    userId: 16,
+    house: 'SheepHouse'
+  },
+  {
+    name: 'clean the side bathroom',
+    details: 'it is a mess!',
+    userId: 17,
+    house: 'DogHouse'
+  },
+  {
+    name: 'clean the back bathroom',
+    details: 'it is a mess!',
+    userId: 18,
+    house: 'CatHouse'
+  },
+  {
+    name: 'clean the guest bathroom',
+    details: 'it is a mess!',
+    userId: 19,
+    house: 'BirdHouse'
+  },
+  {
+    name: 'clean the master bathroom',
+    details: 'it is a mess!',
+    userId: 20,
+    house: 'BugHouse'
+  },
+  {
+    name: 'clean the upstairs bathroom',
+    details: 'it is a mess!',
+    userId: 21,
+    house: 'MouseHouse'
+  },
+  {
+    name: 'clean the upstairs bedroom',
+    details: 'it is a mess!',
+    userId: 22,
+    house: 'SnakeHouse'
+  },
+  {
+    name: 'clean the gutters',
+    details: 'they need it!',
+    userId: 23,
+    house: 'FrogHouse'
+  },
+  {
+    name: 'paint the house',
+    details: 'it is pretty ugly',
+    userId: 24,
+    house: 'SheepHouse'
+  },
+  {
+    name: 'paint the garage',
+    details: 'it is pretty ugly',
+    userId: 25,
+    house: 'DogHouse'
+  },
+  {
+    name: 'paint the mailbox',
+    details: 'it is pretty ugly',
+    userId: 26,
+    house: 'CatHouse'
+  },
+  {
+    name: 'fix the mailbox',
+    details: 'before the mailman comes!',
+    userId: 27,
+    house: 'BirdHouse'
+  },
+  {
+    name: 'dust the furniture',
+    details: 'in the living room',
+    userId: 28,
+    house: 'BugHouse'
+  },
+  {
+    name: 'dust the furniture',
+    details: 'in the office',
+    userId: 29,
+    house: 'MouseHouse'
+  },
+  {
+    name: 'dust the furniture',
+    details: 'in the parlor',
+    userId: 30,
+    house: 'SnakeHouse'
+  },
+  {
+    name: 'empty the dishwasher',
+    details: 'it is VERY full!',
+    userId: 31,
+    house: 'FrogHouse'
+  }
+];
---
+++ data/houses.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { name: 'DogHouse', secretKey: 'oiuhasrfou'},
+  { name: 'CatHouse', secretKey: '234tewrf'},
+  { name: 'BirdHouse', secretKey: '098ht4t'},
+  { name: 'BugHouse', secretKey: '09q58ugronfs'},
+  { name: 'MouseHouse', secretKey: '0483utrfjenfv'},
+  { name: 'SnakeHouse', secretKey: '34518234j'},
+  { name: 'FrogHouse', secretKey: 'oiajfoiuw'},
+  { name: 'SheepHouse', secretKey: '08935jneg'}
+];
