1ae0b0bdb01ffe01f8a91ccd54920fe162930c94
Lola Brenner <amandadbrenner@gmail.com>
2017-12-14 14:45:37 -0700

    adds create item functions

--- db/seeds/dev/houses.js
+++ db/seeds/dev/houses.js
@@ -7,9 +7,42 @@ const bulletinsData = require('../../../data/bulletins');
 const createUser = (knex, user, house) => {
   return knex('houses').where('name', house).first()
     .then(houseRecord => {
-      console.log(user, houseRecord.id);
       return knex('users').insert({
         name: user.name,
+        houseId: houseRecord.id
+      });
+    });
+};
+
+const createBill = (knex, bill, house) => {
+  return knex('houses').where('name', house).first()
+    .then(houseRecord => {
+      return knex('bills').insert({
+        name: bill.name,
+        total: bill.total,
+        dueDate: bill.dueDate,
+        houseId: houseRecord.id
+      });
+    });
+};
+
+const createBulletin = (knex, bulletin, house) => {
+  return knex('houses').where('name', house).first()
+    .then(houseRecord => {
+      return knex('bulletins').insert({
+        title: bulletin.title,
+        body: bulletin.body,
+        houseId: houseRecord.id
+      });
+    });
+};
+
+const createChore = (knex, chore, house) => {
+  return knex('houses').where('name', house).first()
+    .then(houseRecord => {
+      return knex('chores').insert({
+        name: chore.name,
+        details: chore.details,
         houseId: houseRecord.id
       });
     });
@@ -28,7 +61,7 @@ exports.seed = function(knex, Promise) {
       let usersPromises = [];
       usersData.forEach((user) => {
         let house = user.house;
-        usersPromises.push(createItem(knex, user, house, 'users'));
+        usersPromises.push(createUser(knex, user, house));
       });
 
       return Promise.all(usersPromises);
@@ -37,7 +70,7 @@ exports.seed = function(knex, Promise) {
       let billsPromises = [];
       billsData.forEach((bill) => {
         let house = bill.house;
-        billsPromises.push(createItem(knex, bill, house, 'bills'));
+        billsPromises.push(createBill(knex, bill, house));
       });
 
       return Promise.all(billsPromises);
@@ -46,7 +79,7 @@ exports.seed = function(knex, Promise) {
       let bulletinsPromises = [];
       bulletinsData.forEach((bulletin) => {
         let house = bulletin.house;
-        bulletinsPromises.push(createItem(knex, bulletin, house, 'bulletins'));
+        bulletinsPromises.push(createBulletin(knex, bulletin, house));
       });
 
       return Promise.all(bulletinsPromises);
@@ -55,14 +88,11 @@ exports.seed = function(knex, Promise) {
       let choresPromises = [];
       choresData.forEach((chore) => {
         let house = chore.house;
-        choresPromises.push(createItem(knex, chore, house, 'chores'));
+        choresPromises.push(createChore(knex, chore, house));
       });
 
       return Promise.all(choresPromises);
     })
     .then(() => console.log('Dev Seeding Complete!'))
     .catch(error => console.log({ error }));
-  // ]);//closes Promise.all//
 };
-//     .catch(error => console.log({ error }));
-// };
